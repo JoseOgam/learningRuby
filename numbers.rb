35ddb61b825f5e89e0e5c53e4b259dd5c0dbc6d3
JoseOgam <jotienoochieng@gmail.com>
2020-03-09 15:31:16 +0300

    Ruby  numbers, string and  variable

--- numbers.rb
+++ numbers.rb
@@ -14,3 +14,28 @@ puts 2 * 3
 puts 3 / 2
 puts 10 - 11
 puts 1.5 / 2.6
+
+puts (5 % 3) # prints  2
+puts (-5 % 3) # prints  1
+puts (5 % -3) # prints -1
+puts (-5 % -3) # prints -2
+
+puts "nil" || 2020
+puts false || 2020
+puts "ruby" || 2020
+
+def g *args # The splat here says accept 1 or more arguments, in the form of an Array
+  args      # This returns an array
+end
+
+def f arg
+  arg
+end
+
+x,y,z = [true, 'two', false] # parrallel assignment lets us do this
+
+if a = f(x) and b = f(y) and c = f(z) then
+  d = g(a,b,c) # An array is returned, and stored in variable d
+end
+
+p b # using p to puts and inspect b
